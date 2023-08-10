b0c4cacf74a127c91e69743573a7930c78e42682
absoftlab99 <abdullahtech2@outlook.com>
2023-08-10 12:54:14 +0600

    profile Update

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div>
-            Profile
+        <div className="md:flex gap-4 w-full py-8 px-4 md:px-8">
+            <div className="grid py-4 flex-grow card bg-base-300 rounded-box place-items-center my-2">
+                <h1 className='text-2xl'>আমার প্রোফাইল</h1>
+                <img className='rounded-full w-28 h-28' src="https://i.ibb.co/X88tbV7/my-photo3.jpg" alt="Abdullah Al Mahmud" />
+            </div> 
+            <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center my-2">content</div>
         </div>
     );
 };
